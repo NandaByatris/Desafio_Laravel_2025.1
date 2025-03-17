04839a5a46e02e58e22a299c878e3bd5bd250b60
NandaByatris <fg693520@gmail.com>
2025-03-16 23:59:28 -0300

    atualiza

---
+++ database/factories/AdminFactory.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Admin;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\Admini;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class AdminFactory extends Factory
+{
+    
+    /**
+     * 
+     *
+     * @var string
+     */
+    protected $model = Admin::class;
+
+    /**
+     * 
+     *
+     * @return array
+     */
+  
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'endereco' => $this->faker->address(),
+            'telefone' => $this->faker->phoneNumber(),
+            'password' => Hash::make('password123'),
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
